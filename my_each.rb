b0da33f3c3ed376684ba3c3c18fb4ba3e0be7087
Nina Angelo <ninacelie@gmail.com>
2019-12-11 21:12:11 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,29 +1,8 @@
 def my_each(array)
   i = 0
   while i < array.length
-    yield(array[i])
+    yield array[i]
     i = i + 1
   end
-end
-
-def my_collect(array)
-   i = 0
-   new = []
-   while i < array.length
-      new << yield(array[i])
-      i += 1
-   end
-   new
-end
-
-def my_select(collection)
-   i = 0
-   new = []
-   while i < collection.length
-      if yield(collection[i]) == true
-         new << collection[i]
-      end
-      i += 1
-   end
-   new
+  array
 end
